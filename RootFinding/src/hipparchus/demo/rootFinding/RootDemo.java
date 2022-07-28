3b96c597729e7bb2a74ee4ed7966bb54fae7cf0d
Andreas Wassmer <andreas.wassmer@me.com>
2022-07-28 15:28:19 +0200

    Project cleanup

--- RootFinding/src/hipparchus/demo/rootFinding/RootDemo.java
+++ RootFinding/src/hipparchus/demo/rootFinding/RootDemo.java
@@ -1,19 +1,22 @@
 package hipparchus.demo.rootFinding;
 
 import org.hipparchus.analysis.UnivariateFunction;
-import org.hipparchus.analysis.solvers.AllowedSolution;
 import org.hipparchus.analysis.solvers.BracketingNthOrderBrentSolver;
 import org.hipparchus.analysis.solvers.UnivariateSolver;
 
 public class RootDemo {
-
   public static void main(String[] args) {
     UnivariateFunction function = x -> 2*x*x-5.0;
+    brentSolverDemo(function);
+  }
+
+  private static void brentSolverDemo(UnivariateFunction function) {
     final double relativeAccuracy = 1.0e-12;
     final double absoluteAccuracy = 1.0e-8;
     final int    maxOrder         = 5;
+
     UnivariateSolver solver = new BracketingNthOrderBrentSolver(relativeAccuracy, absoluteAccuracy, maxOrder);
     double c = solver.solve(100, function, 0.0, 5.0);
-    System.out.println(c);
+    System.out.println("Solution (Brent Solver) = " + c);
   }
 }
