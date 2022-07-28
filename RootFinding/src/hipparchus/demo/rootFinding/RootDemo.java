bae814d1f0de1ecddc38fc32e62963845f94fd4f
Andreas Wassmer <andreas.wassmer@me.com>
2022-07-28 15:42:27 +0200

    Root finding demos

--- RootFinding/src/hipparchus/demo/rootFinding/RootDemo.java
+++ RootFinding/src/hipparchus/demo/rootFinding/RootDemo.java
@@ -2,15 +2,21 @@ package hipparchus.demo.rootFinding;
 
 import org.hipparchus.analysis.UnivariateFunction;
 import org.hipparchus.analysis.solvers.BracketingNthOrderBrentSolver;
+import org.hipparchus.analysis.solvers.RegulaFalsiSolver;
 import org.hipparchus.analysis.solvers.UnivariateSolver;
 
 public class RootDemo {
   public static void main(String[] args) {
     UnivariateFunction function = x -> 2*x*x-5.0;
-    brentSolverDemo(function);
+
+    System.out.println("Bracketing Solver");
+    bracketingSolverDemo(function);
+
+    System.out.println("\nRegula Falsi Solver");
+    regulaFalsiDemo(function);
   }
 
-  private static void brentSolverDemo(UnivariateFunction function) {
+  private static void bracketingSolverDemo(UnivariateFunction function) {
     final double relativeAccuracy = 1.0e-12;
     final double absoluteAccuracy = 1.0e-8;
     final int    maxOrder         = 5;
@@ -18,5 +24,15 @@ public class RootDemo {
     UnivariateSolver solver = new BracketingNthOrderBrentSolver(relativeAccuracy, absoluteAccuracy, maxOrder);
     double c = solver.solve(100, function, 0.0, 5.0);
     System.out.println("Solution (Brent Solver) = " + c);
+  }
+
+  private static void regulaFalsiDemo(UnivariateFunction function) {
+    final double relativeAccuracy = 1.0e-12;
+    final double absoluteAccuracy = 1.0e-8;
+
+    UnivariateSolver solver = new RegulaFalsiSolver(relativeAccuracy, absoluteAccuracy);
+    double c = solver.solve(100, function, 0.0, 5.0);
+    System.out.println("Solution (Regula Falsi Solver) = " + c);
+
   }
 }
